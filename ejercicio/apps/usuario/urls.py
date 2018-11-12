98befe0d3ae89dfe37de46876575f5b61c57230e
Jocelyn <jogubaez@hotmail.com>
2018-11-11 21:42:10 -0300

    Listar usuarios

--- ejercicio/apps/usuario/urls.py
+++ ejercicio/apps/usuario/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
-from apps.usuario.views import index
+from apps.usuario.views import index, user_list
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^userlist/', user_list, name='user_listar'),
 ]
+
--- ejercicio/apps/usuario/views.py
+++ ejercicio/apps/usuario/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.models import User
 # Create your views here.
 
 
 def index(request):
     return render(request, 'principal/index.html')
+
+def user_list(request):
+    user = User.objects.all()
+    contexto = {'users':user}
+    return render(request, 'registration/user_list.html', contexto)    
